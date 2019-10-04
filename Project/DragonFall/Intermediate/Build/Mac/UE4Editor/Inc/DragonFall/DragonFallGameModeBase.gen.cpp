58a2885cab473e848fa12d458c70060631a416c3
Blake Erquiaga <cbcerquiaga@me.com>
2019-10-04 18:06:04 -0500

    Initialized project
    This is just an empty C++ UE4 project with nothing done to it yet.

---
+++ Project/DragonFall/Intermediate/Build/Mac/UE4Editor/Inc/DragonFall/DragonFallGameModeBase.gen.cpp
@@ -0,0 +1,69 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "DragonFallGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeDragonFallGameModeBase() {}
+// Cross Module References
+	DRAGONFALL_API UClass* Z_Construct_UClass_ADragonFallGameModeBase_NoRegister();
+	DRAGONFALL_API UClass* Z_Construct_UClass_ADragonFallGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_DragonFall();
+// End Cross Module References
+	void ADragonFallGameModeBase::StaticRegisterNativesADragonFallGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_ADragonFallGameModeBase_NoRegister()
+	{
+		return ADragonFallGameModeBase::StaticClass();
+	}
+	UClass* Z_Construct_UClass_ADragonFallGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+				(UObject* (*)())Z_Construct_UPackage__Script_DragonFall,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+				{ "IncludePath", "DragonFallGameModeBase.h" },
+				{ "ModuleRelativePath", "DragonFallGameModeBase.h" },
+				{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+			};
+#endif
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<ADragonFallGameModeBase>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&ADragonFallGameModeBase::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00900288u,
+				nullptr, 0,
+				nullptr, 0,
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ADragonFallGameModeBase, 1098721079);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ADragonFallGameModeBase(Z_Construct_UClass_ADragonFallGameModeBase, &ADragonFallGameModeBase::StaticClass, TEXT("/Script/DragonFall"), TEXT("ADragonFallGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ADragonFallGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ Project/DragonFall/Source/DragonFall/DragonFall.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DragonFall.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DragonFall, "DragonFall" );
